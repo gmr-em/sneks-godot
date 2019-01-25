dd0618d85ca56eaf43ddf162961228a6a4dd0dd1
MariusG <mg@example.com>
2019-01-25 22:16:37 +0100

    Added single player and multiPlayer scenes; Added Retry and Quit buttons

--- CollisionShape2D.gd
+++ CollisionShape2D.gd
@@ -1,9 +1,10 @@
 extends Area2D
 
 export(PackedScene) var tailScene
+export var player = 0
 
 var speed = 120
-var rotationSpeed = PI/2
+var rotationSpeed = PI/1.1
 var direction = Vector2(0,-1)
 
 var leftLimit = 200
@@ -11,40 +12,75 @@ var rightLimit = 800
 var topLimit = 80
 var bottomLimit = 480
 
+var waitTime = 0.1
+var ate = false
+
 var tail = []
+
+var moveRightPressed = false
+var moveLeftPressed = false
+var some = ""
+
+var RIGHT = KEY_RIGHT
+var LEFT = KEY_LEFT
 
 func _ready():
 	leftLimit = get_node("/root/root/Arena/Left").position.x
 	rightLimit = get_node("/root/root/Arena/Right").position.x
 	topLimit = get_node("/root/root/Arena/Top").position.y
 	bottomLimit = get_node("/root/root/Arena/Bottom").position.y
-	for i in range(4):
-		var newTail = tailScene.instance()
-		newTail.position = Vector2(self.position.x, self.position.y)
-		newTail.get_node("CollisionShape2D").disabled = true
-		get_node("..").call_deferred("add_child", newTail)
-		tail.append(newTail)	
 	
+	get_parent().connect("resetHead", self, "_on_reset")
+	reset()
+	
+	if (player == 2):
+		RIGHT = KEY_D
+		LEFT = KEY_A
+	
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	var timer = Timer.new()
+	timer.connect("timeout", self, "moveSnek") 
+	timer.wait_time = waitTime
+	#timeout is what says in docs, in signals
+	#self is who respond to the callback
+	#_on_timer_timeout is the callback, can have any name
+	add_child(timer) #to process
+	timer.start() #to start
 
-func _process(delta):
+func moveSnek():
+	var delta = waitTime	
 	# Called every frame. Delta is time since last frame.
 	# Update game logic here.		
-	if Input.is_key_pressed(KEY_RIGHT):
+	if Input.is_key_pressed(RIGHT) or moveRightPressed:
 		direction = direction.rotated(-rotationSpeed * delta)
-	if Input.is_key_pressed(KEY_LEFT):
+	if Input.is_key_pressed(LEFT) or moveLeftPressed:
 		direction = direction.rotated(rotationSpeed * delta)
 	
-	var tailEnd = tail.pop_back()
-	tailEnd.position = Vector2(self.position.x, self.position.y)
-	tailEnd.get_node("CollisionShape2D").disabled = true
-	tail.push_front(tailEnd)
+	# if we just ate then create a new tail object in the current position of the head (head will move later)
+	if ate:
+		ate = false
+		var newTail = tailScene.instance()
+		newTail.get_node("CollisionShape2D").disabled = true
+		newTail.position = Vector2(self.position.x, self.position.y)		
+		get_node("..").call_deferred("add_child", newTail)
+		tail.push_front(newTail)
+		# also increment the score
+		incrementScore()
+		
+	else: # if we didn't eat then take the last tail piece and move it to the current pos of the head (head will move later)
+		var tailEnd = tail.pop_back()
+		tailEnd.get_node("CollisionShape2D").disabled = true
+		tailEnd.position = Vector2(self.position.x, self.position.y)		
+		tail.push_front(tailEnd)	
+
+	# move head in the forward direction
+	self.move_local_y(direction.x * speed * delta)
+	self.move_local_x(direction.y * speed * delta)
 	
 	if tail.size() > 3 :
 		var tail4 = tail[3]
 		tail4.get_node("CollisionShape2D").disabled = false
-
-	self.move_local_y(direction.x * speed * delta)
-	self.move_local_x(direction.y * speed * delta)
 	
 	if self.position.x > rightLimit:
 		self.position.x = leftLimit
@@ -55,12 +91,55 @@ func _process(delta):
 	if self.position.y < topLimit:
 		self.position.y = bottomLimit
 
-#	if Input.is_key_pressed(KEY_UP):
-#		self.move_local_y(-5)
-#	if Input.is_key_pressed(KEY_DOWN):
-#		self.move_local_y(5)
-
-
 func _on_Area2D_area_entered(area):
-	print ("Entered")
-	#area.queue_free()
+	if (area.get_parent().get_name() == "FoodBasket"):
+		area.queue_free() 
+		ate = true
+	else:
+		get_parent().dead()
+		
+
+func _on_MoveRight_button_up():
+	moveRightPressed = false
+	pass # replace with function body
+
+func _on_MoveRight_button_down():
+	moveRightPressed = true
+	pass # replace with function body
+
+func _on_MoveLeft_button_down():
+	moveLeftPressed = true
+	pass # replace with function body
+
+func _on_MoveLeft_button_up():
+	moveLeftPressed = false
+	pass # replace with function body
+
+func incrementScore():
+	var score = get_node("/root/root/UI"+String(player)+"/Score")
+	score.text = "Score: " + (String (tail.size() - 3))
+	
+func _on_reset():
+	reset()
+	
+func reset():
+	ate = false
+	for tailPiece in tail:
+		tailPiece.queue_free()
+	tail.clear()
+	
+	
+	self.position.y = 300
+	if (player == 0):
+		self.position.x = 500		
+	else:
+		self.position.x = 300 * player^2
+	self.direction.x = -1
+	self.direction.y = 0
+	
+	for i in range(3):
+		var newTail = tailScene.instance()
+		newTail.position = Vector2(self.position.x, self.position.y)
+		newTail.get_node("CollisionShape2D").disabled = true
+		get_node("..").call_deferred("add_child", newTail)
+		tail.append(newTail)
--- root.gd
+++ root.gd
@@ -8,11 +8,23 @@ var topLimit = 80
 var bottomLimit = 480
 var margin = 10
 
+signal resetHead
+
 func _ready():
 	leftLimit = get_node("/root/root/Arena/Left").position.x + margin
 	rightLimit = get_node("/root/root/Arena/Right").position.x - margin
 	topLimit = get_node("/root/root/Arena/Top").position.y + margin
 	bottomLimit = get_node("/root/root/Arena/Bottom").position.y - margin
+	
+	var FoodBasket = Node.new()
+	FoodBasket.name = "FoodBasket"
+	add_child(FoodBasket)
+	
+	var retry = get_node("gameOverPopup/Retry")
+	retry.connect("pressed", self, "_on_Retry_pressed")
+	
+	var quit = get_node("gameOverPopup/Quit")
+	quit.connect("pressed", self, "_on_Quit_pressed")
 	
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
@@ -33,7 +45,30 @@ func _process(delta):
 func generateFood():
 	var new_food = food.instance()
 	new_food.position = Vector2(rand_range(rightLimit, leftLimit), rand_range(topLimit, bottomLimit))
-	add_child(new_food)
+	get_node("FoodBasket").add_child(new_food)
+	
+
+func _on_Retry_pressed():
+	var basket = get_node("FoodBasket")
+	
+	for oldFood in basket.get_children():
+		oldFood.queue_free()
+	
+#	get_node("Head").reset()
+	emit_signal("resetHead")
+	get_node("gameOverPopup").hide()
+	get_tree().set_pause(false)
 	
 	
-	
+func dead():
+	get_node("gameOverPopup").set_exclusive(true)
+	get_node("gameOverPopup").popup()
+	get_tree().set_pause(true)
+
+
+func _on_Button_pressed():
+	dead()
+
+func _on_Quit_pressed():
+	get_tree().set_pause(false)
+	get_tree().change_scene("res://mainMenu.tscn")
